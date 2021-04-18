95f54f2d99f024fb0ca8f7721b42efa17c3ac2a9
griffin-stewie <panterathefamilyguy@gmail.com>
2021-04-18 14:21:52 +0900

    support integers

--- Sources/LTSV/LTSVDecoder.swift
+++ Sources/LTSV/LTSVDecoder.swift
@@ -119,7 +119,7 @@ private struct LTSVKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContaine
     }
 
     func decode(_ type: Int.Type, forKey key: Key) throws -> Int {
-        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = Int(s) else {
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
             throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
         }
 
@@ -131,7 +131,7 @@ private struct LTSVKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContaine
             throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
         }
 
-        guard let unwrapped = s, let v = Int(unwrapped) else {
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
             return nil
         }
 
@@ -139,40 +139,185 @@ private struct LTSVKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContaine
     }
 
     func decode(_ type: Int8.Type, forKey key: Key) throws -> Int8 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: Int8.Type, forKey key: Key) throws -> Int8? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: Int16.Type, forKey key: Key) throws -> Int16 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: Int16.Type, forKey key: Key) throws -> Int16? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: Int32.Type, forKey key: Key) throws -> Int32 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: Int32.Type, forKey key: Key) throws -> Int32? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: Int64.Type, forKey key: Key) throws -> Int64 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: Int64.Type, forKey key: Key) throws -> Int64? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: UInt.Type, forKey key: Key) throws -> UInt {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: UInt.Type, forKey key: Key) throws -> UInt? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: UInt8.Type, forKey key: Key) throws -> UInt8 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: UInt8.Type, forKey key: Key) throws -> UInt8? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: UInt16.Type, forKey key: Key) throws -> UInt16 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: UInt16.Type, forKey key: Key) throws -> UInt16? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: UInt32.Type, forKey key: Key) throws -> UInt32 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
+    }
+
+    func decodeIfPresent(_ type: UInt32.Type, forKey key: Key) throws -> UInt32? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
     }
 
     func decode(_ type: UInt64.Type, forKey key: Key) throws -> UInt64 {
-        fatalError("not implemented")
+        guard let s = self.container[key.stringValue]?.flatMap({$0}), let v = type.init(s) else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        return v
     }
+
+    func decodeIfPresent(_ type: UInt64.Type, forKey key: Key) throws -> UInt64? {
+        guard let s = self.container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "TODO"))
+        }
+
+        guard let unwrapped = s, let v = type.init(unwrapped) else {
+            return nil
+        }
+
+        return v
+    }
+
 
     func decode<T>(_ type: T.Type, forKey key: Key) throws -> T where T : Decodable {
         fatalError("not implemented")
--- Tests/LTSVTests/LTSVDecoderTests.swift
+++ Tests/LTSVTests/LTSVDecoderTests.swift
@@ -52,31 +52,62 @@ final class LTSVDecoderTests: XCTestCase {
         XCTAssertEqual(model.label2!, "value2")
     }
 
-    func testLTSVDecodeRowsSupoortInt() throws {
+    func testLTSVDecodeRowsSupoortIntFamily() throws {
         struct Model: Codable {
-            let label1: String?
-            let label2: String?
-            let status: Int
-            let size: Int?
+            let int: Int
+            let intOptional: Int?
+            let int8: Int8
+            let int8Optional: Int8?
+            let int16: Int16
+            let int16Optional: Int16?
+            let int32: Int32
+            let int32Optional: Int32?
+            let int64: Int64
+            let int64Optional: Int64?
+            let uInt: UInt
+            let uIntOptional: UInt?
+            let uInt8: UInt8
+            let uInt8Optional: UInt8?
+            let uInt16: UInt16
+            let uInt16Optional: UInt16?
+            let uInt32: UInt32
+            let uInt32Optional: UInt32?
+            let uInt64: UInt64
+            let uInt64Optional: UInt64?
         }
 
-        let string = "label1:\tlabel2:value2\tstatus:200\tsize:"
+        let string = "int:1\tintOptional:\tint8:1\tint8Optional:\tint16:1\tint16Optional:\tint32:1\tint32Optional:\tint64:1\tint64Optional:\tuInt:1\tuIntOptional:\tuInt8:1\tuInt8Optional:\tuInt16:1\tuInt16Optional:\tuInt32:1\tuInt32Optional:\tuInt64:1\tuInt64Optional:"
 
         let decoder = LTSVDecoder()
         let model = try decoder.decode(Model.self, from: string)
 
-        XCTAssertNil(model.label1)
-        XCTAssertEqual(model.label2!, "value2")
-        XCTAssertEqual(model.status, 200)
-        XCTAssertNil(model.size)
+        XCTAssertEqual(model.int, 1)
+        XCTAssertNil(model.intOptional)
+        XCTAssertEqual(model.int8, 1)
+        XCTAssertNil(model.int8Optional)
+        XCTAssertEqual(model.int16, 1)
+        XCTAssertNil(model.int16Optional)
+        XCTAssertEqual(model.int32, 1)
+        XCTAssertNil(model.int32Optional)
+        XCTAssertEqual(model.int64, 1)
+        XCTAssertNil(model.int64Optional)
+        XCTAssertEqual(model.uInt, 1)
+        XCTAssertNil(model.uIntOptional)
+        XCTAssertEqual(model.uInt8, 1)
+        XCTAssertNil(model.uInt8Optional)
+        XCTAssertEqual(model.uInt16, 1)
+        XCTAssertNil(model.uInt16Optional)
+        XCTAssertEqual(model.uInt32, 1)
+        XCTAssertNil(model.uInt32Optional)
+        XCTAssertEqual(model.uInt64, 1)
+        XCTAssertNil(model.uInt64Optional)
     }
-
 
     static var allTests = [
         ("testLTSVDecodeSingleRow", testLTSVDecodeSingleRow),
         ("testLTSVDecodeRows", testLTSVDecodeRows),
         ("testLTSVDecodeRowsTheEmptyValueFieldAsNil", testLTSVDecodeRowsTheEmptyValueFieldAsNil),
-        ("testLTSVDecodeRowsSupoortInt", testLTSVDecodeRowsSupoortInt),
+        ("testLTSVDecodeRowsSupoortIntFamily", testLTSVDecodeRowsSupoortIntFamily),
     ]
 }
 
